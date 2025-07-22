c65c429dfd74bc4abdd827a99f5290a9ed69c05d
Jet <>
2025-07-22 13:27:23 +0800

    update

--- app/src/main/java/com/szr/co/smart/qr/constant/Constants.kt
+++ app/src/main/java/com/szr/co/smart/qr/constant/Constants.kt
@@ -3,5 +3,5 @@ package com.szr.co.smart.qr.constant
 object Constants {
     val pushListId = arrayOf(77433, 77434, 77435, 77436, 77437, 77438)
 
-    const val SE_APP_ID = ""
+    const val SE_APP_ID = "afedaad9cf3e17b9"
 }
